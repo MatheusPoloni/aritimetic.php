9ad201f9f85138a1b1762d71499ddde5fa76123b
Matheus Machado <100002013+MatheusPoloni@users.noreply.github.com>
2024-02-26 14:14:26 -0300

    Add files via upload

---
+++ aritimetico.php
@@ -0,0 +1,42 @@
+<?php
+//Comentários de uma linha são realizados com a utilização de barras duplas
+
+/*
+Cada comando deve ser terminado com ;
+Abaixo temos alguns exemplos de término de comandos
+*/
+
+//Subtração
+echo "Teste aritimético\n";
+$var1 = 2024 - 2006;
+
+echo "Eu tenho " . $var1; echo " anos\n";
+
+//Adição
+$var2 = 17 + 13 ;
+
+echo "O Corinthians tem: " . $var2; echo " Campeonatos Paulista\r"; 
+
+//Multiplicação
+$var3 = 71 * 27 ;
+
+echo "A primeira guerra mundial foi iniciada em " . $var3; 
+echo "\r";
+
+//Divisão
+$var4 = 56 / 4 ;
+
+echo "O Real Madrid tem " . $var4; echo " Champions League\n";
+
+//Modulo
+$var5 = 11 % 2;
+
+echo "Resolução:" . $var5;
+echo "\r";
+
+//Exponencial
+$var6 = 9 ** 2;
+
+echo "Em dezembro de " . $var6; echo " Botou os ingleses na roda 3 a 0 no Liverpool Ficou marcado na história";
+// o \r ou \n pula linha
+// quando só tem PHP no código não precisa fechar a tag
